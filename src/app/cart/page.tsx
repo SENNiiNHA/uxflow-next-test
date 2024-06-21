6fd053c23fe131b4967fac88472c366c2f8cb5d5
Pedro Senna <pedro.senna@scaleup.com.br>
2024-06-21 02:53:01 -0300

    cart kickstart

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default async function Cart() {
+
+  return <div className="w-full flex justify-center">
+          <div className="w-full max-w-[1232px] flex items-center justify-center pt-[52px] gap-10">
+
+          </div>
+        </div>;
+}
