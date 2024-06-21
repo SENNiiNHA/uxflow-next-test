33e793bd57b62ed85586e01354dea797aeb9726d
Pedro Senna <pedro.senna@scaleup.com.br>
2024-06-20 21:00:17 -0300

    list page with styled stuff, preparing to turn it all into components.

---
+++ src/assets/index.ts
@@ -0,0 +1,12 @@
+//Images
+export {default as Logo} from './img/flowlogo.svg';
+export {default as Success} from './img/success.svg';
+
+//Icons
+export {default as CheckCircle} from './icon/checkcircle.svg';
+export {default as EyeOpen} from './icon/eyeopen.svg';
+export {default as Pix} from './icon/pix.svg';
+export {default as Search} from './icon/search.svg';
+export {default as ShoppingBag} from './icon/shoppingbagopen.svg';
+export {default as Trash} from './icon/trashsimple.svg';
+export {default as User} from './icon/user.svg';
