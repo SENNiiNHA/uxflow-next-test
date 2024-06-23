bc3174bd3ab66ab8f048e66d7d9d93357adf124b
Pedro Senna <pedro.senna@scaleup.com.br>
2024-06-23 18:48:08 -0300

    new pricing structure to test out the product listing.

--- src/components/molecules/ProductCard.tsx
+++ src/components/molecules/ProductCard.tsx
@@ -9,6 +9,12 @@ type ProductCardProps = {
 };
 
 const ProductCard: React.FC<ProductCardProps> = ({product}) => {
+  function handleCurrencyFormat(price: number, discount?: number) {
+    if(!discount) {
+      return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(price);
+    }
+      return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(price - (price * (discount / 100)));
+  }
 
   return (<div className="w-full max-w-[290px] flex flex-col items-center justify-center gap-6">
             <Image src={Shoes01} alt="Shoes" className="w-full h-[330px] rounded-lg"/>
@@ -17,8 +23,8 @@ const ProductCard: React.FC<ProductCardProps> = ({product}) => {
               <p className="text-xl leading-7 font-medium line-clamp-2">{product.title}</p>
             </div>
             <div className="w-full flex flex-col justify-items-start">
-              {product.hasDiscount ? (<p className="text-sm leading-6 text-gray-400 line-through">De R$8.999,00</p>) : null}
-              <p className="text-[28px] leading-[42px] font-semibold flex items-center gap-3">R$ 270,00{product.hasDiscount ? (<span className="text-primary-500 text-sm leading-6 font-normal">&nbsp;10% OFF</span>) : null}</p>
+              {product.hasDiscount ? (<p className="text-sm leading-6 text-gray-400 line-through">De {handleCurrencyFormat(product.price)}</p>) : null}
+              <p className="text-[28px] leading-[42px] font-semibold flex items-center gap-3">{handleCurrencyFormat(product.price, product.hasDiscount ? product.discount : undefined)}{product.hasDiscount ? (<span className="text-primary-500 text-sm leading-6 font-normal">&nbsp;{product.discount}% OFF</span>) : null}</p>
             </div>
             <button className="w-full h-12 bg-white rounded-[4px] text-primary-500 text-sm leading-6 border-[1px] border-primary-500">Adicionar ao carrinho</button>
           </div>)
